2529fc3d9e9ce33817393539be393082142ab9dc
Ellen Park <ellenpark781@gmail.com>
2021-01-12 15:18:40 -0500

    added sounds to drums

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 let btnLength = document.querySelectorAll(".drum").length;
-
 for (let i = 0; i < btnLength; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
-
 }
 
 
 function handleClick() {
-  switch (audio) {
+
+
+  switch (this.innerHTML) {
     case 'w':
-      tom1.play()
+      let tom1 =  new Audio("sounds/tom-1.mp3")
+      tom1.play();
       break;
+
     case 'a': 
-      tom2.play
-    default:
+      let tom2 =  new Audio("sounds/tom-2.mp3")
+      tom2.play();
+      break;
+
+    case 's': 
+      let tom3 =  new Audio("sounds/tom-3.mp3")
+      tom3.play();
+      break;
+
+
+    case 'd': 
+      let tom4 =  new Audio("sounds/tom-4.mp3")
+      tom4.play();
+      break;
+
+    case 'j': 
+      let snare =  new Audio("sounds/snare.mp3")
+      snare.play();
+      break;
+
+    case 'k': 
+      let crash =  new Audio("sounds/crash.mp3")
+      crash.play();
+      break;
+
+    case 'l': 
+      let kick =  new Audio("sounds/kick-bass.mp3")
+      kick.play();
+      break;
+
+    default: 
+    console.log(btnInnerHTML)
       break;
   }
-  
-//   w drum">w</button>
-//   <button class="a drum">a</button>
-//   <button class="s drum">s</button>
-//   <button class="d drum">d</button>
-//   <button class="j drum">j</button>
-//   <button class="k drum">k</button>
-//   <button class="l drum">l</button>
-// </div> 
- tom1.play()
 
 }
 
-let audio = new Audio("sounds/tom-1.mp3")
